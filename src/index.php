f2a72b13d2bdd3f89d61190f4ec9d8590fe53760
darwinas05 <darwinalemansoza.2005@gmail.com>
2024-11-20 16:22:29 +0100

    sin mas la verdad

--- src/index.php
+++ src/index.php
@@ -57,6 +57,8 @@
         </div>
       </div>
 
+
+<!--card que usamos para clonar y usar-->
       <div id="card-ejemplo" class="bg-white">
         <div class="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
           <h2 id="card-title" class="text-2xl font-bold tracking-tight text-gray-900">Customers also purchased</h2>
